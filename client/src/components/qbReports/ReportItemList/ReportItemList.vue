97e0c5a93afd897e967485fd07bb2c8ec80b8b47
aidanwoolcott <aidan@ionrev.com>
2022-08-22 14:55:44 -0600

    Bringing over reports from website-builder to sp-template

---
+++ client/src/components/qbReports/ReportItemList/ReportItemList.vue
@@ -0,0 +1,28 @@
+<template>
+  <report-item class="ReportItemList" :data="data">
+    <template #child="item">
+      <template v-if="item.hasOwnProperty('Rows')">
+        <report-item-list :data="item"/>
+      </template>
+<!--      <report-item v-else :data="item"/>-->
+    </template>
+  </report-item>
+</template>
+
+<script>
+  import ReportItem from 'pages/../../../../components/qbReports/ReportItem/ReportItem';
+  export default {
+    name: 'report-item-list',
+    components: {ReportItem},
+    props: {
+      data: {
+        type: Object,
+        required: true
+      }
+    },
+  };
+</script>
+
+<style lang="scss" scoped src="_ReportItemList.scss">
+
+</style>
