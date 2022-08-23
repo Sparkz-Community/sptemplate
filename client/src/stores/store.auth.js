54470901151ec7766990d422f9939b7268f80af4
dallinjohnson <dallin.b.johnson@gmail.com>
2022-08-23 15:51:02 -0600

    Refactor spAuthmanagementLib.

--- client/src/stores/store.auth.js
+++ client/src/stores/store.auth.js
@@ -1,10 +1,25 @@
-import {Users} from './services/users.js';
-
 import feathersClient from '../api/feathers-client';
 import {stores} from '@sparkz-community/auth-management-client-lib';
 const {auth} = stores;
 
+import {Users} from './services/users.js';
+import {Logins} from './services/logins.js';
+import {Accounts} from './services/accounts.js';
+
 export default auth({
   feathersClient,
   Users,
+  Logins,
+  Accounts,
+  state() {
+    return {
+
+    };
+  },
+  getters: {
+
+  },
+  actions: {
+
+  },
 });
