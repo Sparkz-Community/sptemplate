474e0cca79fb21184ea3df35c51b485f31551aff
jaydenthomas <jayden@ionrev.com>
2022-08-28 13:15:42 -0600

    Refactor PlacesAutoComplete

--- client/src/components/formGeneratorCustom/PlacesAutoComplete/PlacesAutoComplete.vue
+++ client/src/components/formGeneratorCustom/PlacesAutoComplete/PlacesAutoComplete.vue
@@ -1,27 +1,29 @@
 <template>
-  <div id="PlacesAutoComplete" v-bind="$attrs['div-attrs']">
-    <q-form autocomplete="off">
-      <q-select :model-value="input"
-                @input-value="setInput"
-                @update:model-value="geocode"
-                @clear="clearInput"
-                @filter="filterFn"
-                :options="addresses"
-                v-bind="$attrs['attrs']">
-        <template v-for="slot in slots" v-slot:[slot]="slotProps">
-          <slot :name="slot" :key_name="path" v-bind="slotProps"></slot>
-        </template>
+  <transition v-bind="attrs['transition-attrs']">
+    <div id="PlacesAutoComplete" v-bind="attrs['div-attrs']">
+      <q-form autocomplete="off">
+        <q-select :model-value="input"
+                  @input-value="setInput"
+                  @update:model-value="geocode"
+                  @clear="clearInput"
+                  @filter="filterFn"
+                  :options="addresses"
+                  v-bind="attrs['attrs']">
+          <template v-for="slot in slots" v-slot:[slot]="slotProps">
+            <slot :name="slot" :key_name="path" v-bind="slotProps"></slot>
+          </template>
 
-        <template v-if="!slots.includes('no-option')" v-slot:no-option>
-          <q-item>
-            <q-item-section>
-              No results
-            </q-item-section>
-          </q-item>
-        </template>
-      </q-select>
-    </q-form>
-  </div>
+          <template v-if="!slots.includes('no-option')" v-slot:no-option>
+            <q-item>
+              <q-item-section>
+                No results
+              </q-item-section>
+            </q-item>
+          </template>
+        </q-select>
+      </q-form>
+    </div>
+  </transition>
 </template>
 
 <script>
@@ -33,7 +35,7 @@
     name: 'PlacesAutoComplete',
     inheritAttrs: false,
     props: {
-      'model-value': {
+      modelValue: {
         type: [Number, String, Array, Object],
       },
       path: {
@@ -78,34 +80,15 @@
       };
     },
     watch: {
-      $attrs: {
-        immediate: true,
-        deep: true,
-        handler(newVal) {
-          // attrs defaults
-          this.$lset(newVal, 'attrs.filled', this.$lget(newVal, 'attrs.filled', true));
-          this.$lset(newVal, 'attrs.clearable', this.$lget(newVal, 'attrs.clearable', true));
-          this.$lset(newVal, 'attrs.clear-icon', this.$lget(newVal, 'attrs.clear-icon', 'close'));
-          this.$lset(newVal, 'attrs.use-input', this.$lget(newVal, 'attrs.use-input', true));
-          this.$lset(newVal, 'attrs.hide-selected', this.$lget(newVal, 'attrs.hide-selected', true));
-          this.$lset(newVal, 'attrs.fill-input', this.$lget(newVal, 'attrs.fill-input', true));
-          this.$lset(newVal, 'attrs.input-debounce', this.$lget(newVal, 'attrs.input-debounce', '0'));
-          this.$lset(newVal, 'attrs.placeholder', this.$lget(newVal, 'attrs.placeholder', 'Enter your address (ex. 123 N Main St, City, ZZ 12345)...'));
-          this.$lset(newVal, 'attrs.option-label', this.$lget(newVal, 'attrs.option-label', 'description'));
-
-          // div-attrs defaults
-          this.$lset(newVal, 'div-attrs.class', this.$lget(newVal, 'div-attrs.class', 'col-12 col-sm-6'));
-        },
-      },
       '$q.dark.mode': {
         immediate: true,
         handler(newVal, oldVal) {
           if (newVal !== oldVal) {
-            this.$lset(this.$attrs, 'attrs.dark', newVal);
+            this.$lset(this.attrs, 'attrs.dark', newVal);
           }
         }
       },
-      'model-value': {
+      modelValue: {
         deep: true,
         immediate: true,
         // eslint-disable-next-line no-unused-vars
@@ -122,6 +105,25 @@
       ...mapState(usePlacesAutoComplete, {addresses: 'items'}),
       loading() {
         return this.placeloading || this.geoloading;
+      },
+      attrs() {
+        let newVal = {...this.$attrs};
+
+        // attrs defaults
+        this.$lset(newVal, 'attrs.filled', this.$lget(newVal, 'attrs.filled', true));
+        this.$lset(newVal, 'attrs.clearable', this.$lget(newVal, 'attrs.clearable', true));
+        this.$lset(newVal, 'attrs.clear-icon', this.$lget(newVal, 'attrs.clear-icon', 'close'));
+        this.$lset(newVal, 'attrs.use-input', this.$lget(newVal, 'attrs.use-input', true));
+        this.$lset(newVal, 'attrs.hide-selected', this.$lget(newVal, 'attrs.hide-selected', true));
+        this.$lset(newVal, 'attrs.fill-input', this.$lget(newVal, 'attrs.fill-input', true));
+        this.$lset(newVal, 'attrs.input-debounce', this.$lget(newVal, 'attrs.input-debounce', '0'));
+        this.$lset(newVal, 'attrs.placeholder', this.$lget(newVal, 'attrs.placeholder', 'Enter your address (ex. 123 N Main St, City, ZZ 12345)...'));
+        this.$lset(newVal, 'attrs.option-label', this.$lget(newVal, 'attrs.option-label', 'description'));
+
+        // div-attrs defaults
+        this.$lset(newVal, 'div-attrs.class', this.$lget(newVal, 'div-attrs.class', 'col-12 col-sm-6'));
+
+        return newVal;
       },
     },
     methods: {
