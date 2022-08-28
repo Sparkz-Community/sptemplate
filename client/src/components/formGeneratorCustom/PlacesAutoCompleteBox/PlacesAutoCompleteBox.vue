c2fadc8ed5e25c6fbc89ba06099db18d32d572fe
jaydenthomas <jayden@ionrev.com>
2022-08-28 13:21:41 -0600

    Refactor PlacesAutoCompleteBox

--- client/src/components/formGeneratorCustom/PlacesAutoCompleteBox/PlacesAutoCompleteBox.vue
+++ client/src/components/formGeneratorCustom/PlacesAutoCompleteBox/PlacesAutoCompleteBox.vue
@@ -1,59 +1,61 @@
 <template>
-  <div id="PlacesAutoCompleteBox" v-bind="$attrs['div-attrs']">
-    <q-expansion-item v-bind="$attrs['expansion-attrs']">
-      <template v-slot:header="{}">
-        <div style="width: 100%; display: grid; align-items: center;" @[ifClick].stop>
-          <places-auto-complete :model-value="newEditedAddress"
-                                @update:model-value="newEditedAddress = { ...'model-value', ...$event }"
-                                @error="searchInput = ''"
-                                :path="path"
-                                v-bind="$attrs['attrs']"></places-auto-complete>
-        </div>
-      </template>
-
-      <q-card>
-        <q-card-section>
-          <div class="row q-col-gutter-sm">
-            <div v-if="!hideFields.includes('address1')" class="col-12 col-sm-6">
-              <q-input label="Address"
-                       :model-value="newEditedAddress.address1"
-                       disable></q-input>
-            </div>
-            <div v-if="!hideFields.includes('city')" class="col-12 col-sm-6">
-              <q-input label="City"
-                       :model-value="newEditedAddress.city"
-                       disable></q-input>
-            </div>
-            <div v-if="!hideFields.includes('region')" class="col-12 col-sm-6">
-              <q-input label="Region"
-                       :model-value="newEditedAddress.region"
-                       disable></q-input>
-            </div>
-            <div v-if="!hideFields.includes('postal')" class="col-12 col-sm-6">
-              <q-input label="Postal Code"
-                       :model-value="newEditedAddress.postal"
-                       disable></q-input>
-            </div>
-            <div v-if="!hideFields.includes('country')" class="col-12 col-sm-6">
-              <q-input label="Country"
-                       :model-value="newEditedAddress.country"
-                       disable></q-input>
-            </div>
-            <div v-if="!hideFields.includes('latitude')" class="col-12 col-sm-6">
-              <q-input label="Latitude"
-                       :model-value="newEditedAddress.latitude"
-                       disable></q-input>
-            </div>
-            <div v-if="!hideFields.includes('longitude')" class="col-12 col-sm-6">
-              <q-input label="Longitude"
-                       :model-value="newEditedAddress.longitude"
-                       disable></q-input>
-            </div>
+  <transition v-bind="attrs['transition-attrs']">
+    <div id="PlacesAutoCompleteBox" v-bind="attrs['div-attrs']">
+      <q-expansion-item v-bind="attrs['expansion-attrs']">
+        <template v-slot:header="{}">
+          <div style="width: 100%; display: grid; align-items: center;" @[ifClick].stop>
+            <places-auto-complete :model-value="newEditedAddress"
+                                  @update:model-value="newEditedAddress = { ...'model-value', ...$event }"
+                                  @error="searchInput = ''"
+                                  :path="path"
+                                  v-bind="attrs['attrs']"></places-auto-complete>
           </div>
-        </q-card-section>
-      </q-card>
-    </q-expansion-item>
-  </div>
+        </template>
+
+        <q-card>
+          <q-card-section>
+            <div class="row q-col-gutter-sm">
+              <div v-if="!hideFields.includes('address1')" class="col-12 col-sm-6">
+                <q-input label="Address"
+                         :model-value="newEditedAddress.address1"
+                         disable></q-input>
+              </div>
+              <div v-if="!hideFields.includes('city')" class="col-12 col-sm-6">
+                <q-input label="City"
+                         :model-value="newEditedAddress.city"
+                         disable></q-input>
+              </div>
+              <div v-if="!hideFields.includes('region')" class="col-12 col-sm-6">
+                <q-input label="Region"
+                         :model-value="newEditedAddress.region"
+                         disable></q-input>
+              </div>
+              <div v-if="!hideFields.includes('postal')" class="col-12 col-sm-6">
+                <q-input label="Postal Code"
+                         :model-value="newEditedAddress.postal"
+                         disable></q-input>
+              </div>
+              <div v-if="!hideFields.includes('country')" class="col-12 col-sm-6">
+                <q-input label="Country"
+                         :model-value="newEditedAddress.country"
+                         disable></q-input>
+              </div>
+              <div v-if="!hideFields.includes('latitude')" class="col-12 col-sm-6">
+                <q-input label="Latitude"
+                         :model-value="newEditedAddress.latitude"
+                         disable></q-input>
+              </div>
+              <div v-if="!hideFields.includes('longitude')" class="col-12 col-sm-6">
+                <q-input label="Longitude"
+                         :model-value="newEditedAddress.longitude"
+                         disable></q-input>
+              </div>
+            </div>
+          </q-card-section>
+        </q-card>
+      </q-expansion-item>
+    </div>
+  </transition>
 </template>
 
 <script>
@@ -104,32 +106,9 @@
       };
     },
     watch: {
-      $attrs: {
-        immediate: true,
-        deep: true,
-        handler(newVal) {
-          // attrs defaults
-          this.$lset(newVal, 'attrs.attrs.filled', this.$lget(newVal, 'attrs.attrs.filled', false));
-          this.$lset(newVal, 'attrs.attrs.borderless', this.$lget(newVal, 'attrs.attrs.borderless', true));
-          this.$lset(newVal, 'attrs.attrs.dense', this.$lget(newVal, 'attrs.attrs.dense', true));
-          this.$lset(newVal, 'attrs.attrs.popup-content-style', this.$lget(newVal, 'attrs.attrs.popup-content-style', ''));
-
-          // div-attrs defaults
-          this.$lset(newVal, 'div-attrs.class', this.$lget(newVal, 'div-attrs.class', 'col-12 col-sm-6'));
-
-          // expansion-attrs defaults
-          this.$lset(newVal, 'expansion-attrs.header-class', this.$lget(newVal, 'expansion-attrs.header-class', 'bg-primary text-white'));
-          this.$lset(newVal, 'expansion-attrs.expand-icon-class', this.$lget(newVal, 'expansion-attrs.expand-icon-class', 'text-white'));
-          this.$lset(newVal, 'expansion-attrs.style', this.$lget(newVal, 'expansion-attrs.style', 'border-radius: 30px'));
-          this.$lset(newVal, 'expansion-attrs.icon', this.$lget(newVal, 'expansion-attrs.icon', 'explore'));
-          this.$lset(newVal, 'expansion-attrs.class', this.$lget(newVal, 'expansion-attrs.class', 'shadow-1 overflow-hidden'));
-        },
-      },
       newEditedAddress: {
         deep: true,
-        // eslint-disable-next-line no-unused-vars
-        handler(newVal, oldVal) {
-          // console.log('Old: ', oldVal,'New: ', newVal);
+        handler(newVal) {
           if (JSON.stringify(this['model-value']) !== JSON.stringify(newVal)) {
             this.$emit('update:model-value', newVal);
           }
@@ -147,8 +126,34 @@
       },
     },
     computed: {
+      attrs() {
+        let newVal = {...this.$attrs};
+
+        // attrs defaults
+        this.$lset(newVal, 'attrs.attrs.filled', this.$lget(newVal, 'attrs.attrs.filled', false));
+        this.$lset(newVal, 'attrs.attrs.borderless', this.$lget(newVal, 'attrs.attrs.borderless', true));
+        this.$lset(newVal, 'attrs.attrs.dense', this.$lget(newVal, 'attrs.attrs.dense', true));
+        this.$lset(newVal, 'attrs.attrs.popup-content-style',
+                   this.$lget(newVal, 'attrs.attrs.popup-content-style', ''));
+
+        // div-attrs defaults
+        this.$lset(newVal, 'div-attrs.class', this.$lget(newVal, 'div-attrs.class', 'col-12 col-sm-6'));
+
+        // expansion-attrs defaults
+        this.$lset(newVal, 'expansion-attrs.header-class',
+                   this.$lget(newVal, 'expansion-attrs.header-class', 'bg-primary text-white'));
+        this.$lset(newVal, 'expansion-attrs.expand-icon-class',
+                   this.$lget(newVal, 'expansion-attrs.expand-icon-class', 'text-white'));
+        this.$lset(newVal, 'expansion-attrs.style',
+                   this.$lget(newVal, 'expansion-attrs.style', 'border-radius: 30px'));
+        this.$lset(newVal, 'expansion-attrs.icon', this.$lget(newVal, 'expansion-attrs.icon', 'explore'));
+        this.$lset(newVal, 'expansion-attrs.class',
+                   this.$lget(newVal, 'expansion-attrs.class', 'shadow-1 overflow-hidden'));
+
+        return newVal;
+      },
       ifClick() {
-        return this.$lget(this.$attrs, 'attrs.readonly') ? null : 'click';
+        return this.$lget(this.attrs, 'attrs.readonly') ? null : 'click';
       },
     },
   };
