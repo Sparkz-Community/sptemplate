c8d2579548ec65f4995b4e7c0a70ab4092bf0d1f
jaydenthomas <jayden@ionrev.com>
2022-08-28 14:08:06 -0600

    Refactor IconPicker

--- client/src/components/formGeneratorCustom/IconPicker.vue
+++ client/src/components/formGeneratorCustom/IconPicker.vue
@@ -1,35 +1,37 @@
 <template>
-  <div id="IconPicker" v-bind="$attrs['div-attrs']">
-    <q-field :model-value="'model-value'"
-             v-bind="$attrs['q-field-attrs']"
-             @focus="$refs['search'].focus()"
-             @clear="$emit('update:model-value', undefined)">
-      <template v-slot:control>
-        <div class="q-mt-md full-width">
-          <q-select v-model="icon_set" v-bind="$attrs['icon-set-attrs']"/>
-          <q-input v-model="filter" ref="search" v-bind="$attrs['search-attrs']"/>
-          <!--        <p class="q-ma-none" style="font-size: 1.1rem;">-->
-          <!--          <b class="q-mr-sm">Selected:</b>-->
-          <!--          <q-chip v-if="value" removable :icon="value" @remove="$emit('input', undefined)">-->
-          <!--            {{ value }}-->
-          <!--          </q-chip>-->
-          <!--        </p>-->
-          <!--        <q-separator class="q-my-lg"></q-separator>-->
-          <q-icon-picker :model-value="'model-value'"
-                         @update:model-value="$emit('update:model-value', $event)"
-                         :icon-set="icon_set"
-                         v-model:pagination="pagination"
-                         :filter="filter"
-                         :name="path"
-                         v-bind="$attrs['attrs']">
-            <template v-for="slot in slots" v-slot:[slot]="slotProps">
-              <slot :name="slot" :key_name="path" v-bind="slotProps"></slot>
-            </template>
-          </q-icon-picker>
-        </div>
-      </template>
-    </q-field>
-  </div>
+  <transition v-bind="attrs['transition-attrs']">
+    <div id="IconPicker" v-bind="attrs['div-attrs']">
+      <q-field :model-value="modelValue"
+               v-bind="attrs['q-field-attrs']"
+               @focus="$refs['search'].focus()"
+               @clear="$emit('update:model-value', undefined)">
+        <template v-slot:control>
+          <div class="q-mt-md full-width">
+            <q-select v-model="icon_set" v-bind="attrs['icon-set-attrs']"/>
+            <q-input v-model="filter" ref="search" v-bind="attrs['search-attrs']"/>
+            <!--        <p class="q-ma-none" style="font-size: 1.1rem;">-->
+            <!--          <b class="q-mr-sm">Selected:</b>-->
+            <!--          <q-chip v-if="value" removable :icon="value" @remove="$emit('input', undefined)">-->
+            <!--            {{ value }}-->
+            <!--          </q-chip>-->
+            <!--        </p>-->
+            <!--        <q-separator class="q-my-lg"></q-separator>-->
+            <q-icon-picker :model-value="modelValue"
+                           @update:model-value="$emit('update:model-value', $event)"
+                           :icon-set="icon_set"
+                           v-model:pagination="pagination"
+                           :filter="filter"
+                           :name="path"
+                           v-bind="attrs['attrs']">
+              <template v-for="slot in slots" v-slot:[slot]="slotProps">
+                <slot :name="slot" :key_name="path" v-bind="slotProps"></slot>
+              </template>
+            </q-icon-picker>
+          </div>
+        </template>
+      </q-field>
+    </div>
+  </transition>
 </template>
 
 <script>
@@ -37,7 +39,7 @@
     name: 'IconPicker',
     inheritAttrs: false,
     props: {
-      'model-value': {
+      modelValue: {
         type: String,
       },
       path: {
@@ -60,66 +62,65 @@
         },
       };
     },
-    watch: {
-      $attrs: {
-        immediate: true,
-        deep: true,
-        handler(newVal) {
-          // attrs defaults
-          this.$lset(newVal, 'attrs.animated', this.$lget(newVal, 'attrs.animated', true));
-          this.$lset(newVal, 'attrs.style', this.$lget(newVal, 'attrs.style', 'height: 180px;'));
-          this.$lset(newVal, 'attrs.tooltips', this.$lget(newVal, 'attrs.tooltips', true));
-          this.$lset(newVal, 'attrs.selected-background-color', this.$lget(newVal, 'attrs.selected-background-color', 'secondary'));
+    computed: {
+      attrs() {
+        let newVal = {...this.$attrs};
 
-          // q-field-attrs defaults
-          this.$lset(newVal, 'q-field-attrs.label', this.$lget(newVal, 'q-field-attrs.label', 'Select Icon:'));
-          this.$lset(newVal, 'q-field-attrs.filled', this.$lget(newVal, 'q-field-attrs.filled', true));
-          this.$lset(newVal, 'q-field-attrs.clearable', this.$lget(newVal, 'q-field-attrs.clearable', true));
-          this.$lset(newVal, 'q-field-attrs.color', this.$lget(newVal, 'q-field-attrs.color', 'secondary'));
-          if (this.$lget(newVal, 'q-field-attrs.rules')) {
-            this.$lset(newVal, 'q-field-attrs.rules', this.$lget(newVal, 'q-field-attrs.rules', []));
-          }
+        // attrs defaults
+        this.$lset(newVal, 'attrs.animated', this.$lget(newVal, 'attrs.animated', true));
+        this.$lset(newVal, 'attrs.style', this.$lget(newVal, 'attrs.style', 'height: 180px;'));
+        this.$lset(newVal, 'attrs.tooltips', this.$lget(newVal, 'attrs.tooltips', true));
+        this.$lset(newVal, 'attrs.selected-background-color', this.$lget(newVal, 'attrs.selected-background-color', 'secondary'));
 
-          // q-field-attrs validation
-          let rules = this.$lget(newVal, 'q-field-attrs.rules', []);
-          let new_rule = val => {
-            return val && val.length > 0 || `${this.$lget(newVal, 'q-field-attrs.label')} is required`;
-          };
-          if (this.$lget(newVal, 'q-field-attrs.required') && !rules.map(item => item.toString()).includes(new_rule.toString())) {
-            rules.push(new_rule);
-            this.$lset(newVal, 'q-field-attrs.rules', rules);
-          }
+        // q-field-attrs defaults
+        this.$lset(newVal, 'q-field-attrs.label', this.$lget(newVal, 'q-field-attrs.label', 'Select Icon:'));
+        this.$lset(newVal, 'q-field-attrs.filled', this.$lget(newVal, 'q-field-attrs.filled', true));
+        this.$lset(newVal, 'q-field-attrs.clearable', this.$lget(newVal, 'q-field-attrs.clearable', true));
+        this.$lset(newVal, 'q-field-attrs.color', this.$lget(newVal, 'q-field-attrs.color', 'secondary'));
+        if (this.$lget(newVal, 'q-field-attrs.rules')) {
+          this.$lset(newVal, 'q-field-attrs.rules', this.$lget(newVal, 'q-field-attrs.rules', []));
+        }
 
-          // icon-set-attrs defaults
-          this.$lset(newVal, 'icon-set-attrs.label', this.$lget(newVal, 'icon-set-attrs.label', 'Icon Set'));
-          this.$lset(newVal, 'icon-set-attrs.color', this.$lget(newVal, 'icon-set-attrs.color', 'secondary'));
-          // this.$lset(newVal, 'icon-set-attrs.dense', this.$lget(newVal, 'icon-set-attrs.dense', true));
-          this.$lset(newVal, 'icon-set-attrs.options', this.$lget(newVal, 'icon-set-attrs.options', [
-            'ionicons-v4',
-            'fontawesome-v5',
-            'eva-icons',
-            'themify',
-            'line-awesome',
-            'mdi-v5',
-            'material-icons',
-            'material-icons-outlined',
-            'material-icons-round',
-            'material-icons-sharp',
-          ]));
+        // q-field-attrs validation
+        let rules = this.$lget(newVal, 'q-field-attrs.rules', []);
+        let new_rule = val => {
+          return val && val.length > 0 || `${this.$lget(newVal, 'q-field-attrs.label')} is required`;
+        };
+        if (this.$lget(newVal, 'q-field-attrs.required') && !rules.map(item => item.toString()).includes(new_rule.toString())) {
+          rules.push(new_rule);
+          this.$lset(newVal, 'q-field-attrs.rules', rules);
+        }
 
-          // search-attrs defaults
-          this.$lset(newVal, 'search-attrs.label', this.$lget(newVal, 'search-attrs.label', 'Search icons...'));
-          this.$lset(newVal, 'search-attrs.class', this.$lget(newVal, 'search-attrs.class', 'q-my-sm'));
-          this.$lset(newVal, 'search-attrs.color', this.$lget(newVal, 'search-attrs.color', 'secondary'));
-          this.$lset(newVal, 'search-attrs.clearable', this.$lget(newVal, 'search-attrs.clearable', true));
-          // this.$lset(newVal, 'search-attrs.dense', this.$lget(newVal, 'search-attrs.dense', true));
+        // icon-set-attrs defaults
+        this.$lset(newVal, 'icon-set-attrs.label', this.$lget(newVal, 'icon-set-attrs.label', 'Icon Set'));
+        this.$lset(newVal, 'icon-set-attrs.color', this.$lget(newVal, 'icon-set-attrs.color', 'secondary'));
+        // this.$lset(newVal, 'icon-set-attrs.dense', this.$lget(newVal, 'icon-set-attrs.dense', true));
+        this.$lset(newVal, 'icon-set-attrs.options', this.$lget(newVal, 'icon-set-attrs.options', [
+          'ionicons-v4',
+          'fontawesome-v5',
+          'eva-icons',
+          'themify',
+          'line-awesome',
+          'mdi-v5',
+          'material-icons',
+          'material-icons-outlined',
+          'material-icons-round',
+          'material-icons-sharp',
+        ]));
 
-          // div-attrs defaults
-          this.$lset(newVal, 'div-attrs.class', this.$lget(newVal, 'div-attrs.class', 'col-12'));
-        },
+        // search-attrs defaults
+        this.$lset(newVal, 'search-attrs.label', this.$lget(newVal, 'search-attrs.label', 'Search icons...'));
+        this.$lset(newVal, 'search-attrs.class', this.$lget(newVal, 'search-attrs.class', 'q-my-sm'));
+        this.$lset(newVal, 'search-attrs.color', this.$lget(newVal, 'search-attrs.color', 'secondary'));
+        this.$lset(newVal, 'search-attrs.clearable', this.$lget(newVal, 'search-attrs.clearable', true));
+        // this.$lset(newVal, 'search-attrs.dense', this.$lget(newVal, 'search-attrs.dense', true));
+
+        // div-attrs defaults
+        this.$lset(newVal, 'div-attrs.class', this.$lget(newVal, 'div-attrs.class', 'col-12'));
+
+        return newVal;
       },
     },
-    computed: {},
   };
 </script>
 
