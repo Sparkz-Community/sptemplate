d625d33185070eb50d870c41e4ca5c6390ff8593
jaydenthomas <jayden@ionrev.com>
2022-08-29 10:44:29 -0600

    Install vue-sanitize; Copy plugin

--- client/quasar.config.js
+++ client/quasar.config.js
@@ -37,6 +37,7 @@ module.exports = configure(function (ctx) {
       'spRolesLib',
       'plugins/defaults',
       'plugins/treePath',
+      // 'plugins/sanitize',
     ],
 
     // https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-css
