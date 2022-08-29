0320255205513b6f869c355332c5a0cd4b7d2442
aidanwoolcott <aidan@ionrev.com>
2022-08-29 16:19:27 -0600

    Passed Accounts model into common-client-lib

--- client/src/boot/spCommonLib.js
+++ client/src/boot/spCommonLib.js
@@ -1,8 +1,10 @@
 import { boot } from 'quasar/wrappers';
 import spCommonClientLib from '@sparkz-community/common-client-lib';
 
+import useAccountsStore, {Accounts} from 'stores/services/accounts';
+
 export default boot(({ app }) => {
-  app.use(spCommonClientLib);
+  app.use(spCommonClientLib, {useAccountsStore, Accounts});
 });
 
 
